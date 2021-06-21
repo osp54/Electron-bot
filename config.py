b2233f44732c68bee52a7def31b7b4cacfe73a4a
OSP <76648940+osp54@users.noreply.github.com>
2021-06-21 22:22:55 +0300

    Config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+settings = {
+    'token': 'ODI0MzUwMjk3MTQ4NDg5NzM4.YFuGCg.IWi9Ynam2-sBktw5t5JNHZ3Qsds',
+    'bot': 'TerraServer 1.4.2.3',
+    'id': 824350297148489738,
+    'updateSts': 15, #in seconds
+    'prefix': '.'
+}
