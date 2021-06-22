bbfca9b60ba8eafb87dd55a302096b3446bd93cc
OSP <76648940+osp54@users.noreply.github.com>
2021-06-22 14:12:02 +0300

    Added ram, embed color, bot active status.1

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import socket
 import psutil
+import asyncio
 import os
 from config import settings
 from discord.ext import tasks,commands
@@ -17,12 +18,19 @@ class bcolors:
     UNDERLINE = '\033[4m'
 tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
 client = commands.Bot(command_prefix = settings['prefix'])
+async def status_task():
+    while True:
+        await client.change_presence(activity=discord.Game(name=f"CPU = {psutil.cpu_percent()}%"))
+        await asyncio.sleep(10)
+        await client.change_presence(activity=discord.Game(name=f"CPU = {psutil.cpu_percent()}%"))
+        await asyncio.sleep(10)
 @client.command()
 async def status(ctx):
    await ctx.send("status")
 @client.event
 async def on_ready():
     ping.start()
+    client.loop.create_task(status_task())
 @tasks.loop(seconds=settings['updateSts'])
 async def ping():
     def isOpen(ip,port):
@@ -35,12 +43,9 @@ async def ping():
             return False
     server_mobile = isOpen("35.202.253.94", 7777)
     server_pc = isOpen("35.202.253.94", 7776)
-    channel = client.get_channel(856167446195863583)
-    msg = await channel.fetch_message(856174885292998657)
-    emb = discord.Embed( title = "Status")
-    emb.add_field( name = "CPU", value = f"CPU Usage = {psutil.cpu_percent()}% ", inline = False)
-    emb.add_field( name = "RAM", value = f"RAM Usage = {used_m}MB/{tot_m}MB", inline = True)
-    emb.add_field( name = "Servers", value = f"‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍Статус обновляется каждые 15 секунд.\nПоследнее обновление статуса: \n{datetime.now()}", inline = False)
+    channel = client.get_channel(settings['channelid'])
+    msg = await channel.fetch_message(settings['msgid'])
+    emb = discord.Embed( title = "Status", description = f"Статус обновляется каждые 15 секунд.\nПоследнее обновление статуса: \n{datetime.now()}", color = 0x00A725)
     if server_mobile: 
         emb.add_field( name = "Mobile", value = "35.202.253.94:7777    :green_circle: ", inline = False)
     else:
@@ -51,6 +56,8 @@ async def ping():
     else:
         emb.add_field( name = "PC", value = "35.202.253.94:7776   :red_circle: ", inline = False)
         print(f"{bcolors.OKBLUE}{datetime.now()} {bcolors.OKGREEN}| {bcolors.WARNING}{bcolors.BOLD}[Status Servers] {bcolors.OKBLUE}Server PC(35.202.253.94:7776)has down!")
+    emb.add_field( name = "CPU", value = f"CPU Usage = {psutil.cpu_percent()}% ", inline = False)
+    emb.add_field( name = "RAM", value = f"RAM Usage = {used_m}MB/{tot_m}MB", inline = True)
     await msg.edit(embed = emb)
 
 client.run(settings['token'])
