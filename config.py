8bd804dc837c5a696ecd9a0d0a09c8ed9b48c249
OSP <76648940+osp54@users.noreply.github.com>
2021-06-22 14:15:01 +0300

    More Configs.

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ settings = {
     'token': 'ODI0MzUwMjk3MTQ4NDg5NzM4.YFuGCg.IWi9Ynam2-sBktw5t5JNHZ3Qsds',
     'bot': 'TerraServer 1.4.2.3',
     'id': 824350297148489738,
+    'channelid': 856167446195863583,
+    'msgid': 856794513883004948,
     'updateSts': 15, #in seconds
     'prefix': '.'
 }
