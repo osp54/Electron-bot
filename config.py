8b7ee5b94502aad8f8dd670bb06deeaa6e15a227
OSP <76648940+osp54@users.noreply.github.com>
2021-06-26 09:54:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'ODI0MzUwMjk3MTQ4NDg5NzM4.YFuGCg.IWi9Ynam2-sBktw5t5JNHZ3Qsds',
+    'token': '',
     'bot': 'TerraServer 1.4.2.3',
     'id': 824350297148489738,
     'channelid': 856167446195863583,
