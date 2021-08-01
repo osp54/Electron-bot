69d12290bc6ef5ccb7e67a7eddffc83888d5b7b6
OSP <76648940+osp54@users.noreply.github.com>
2021-08-01 15:21:00 +0300

    Fixes

--- cogs/fun.py
+++ cogs/fun.py
@@ -5,10 +5,16 @@ class fun(commands.Cog, name="fun"):
     def __init__(self, bot):
         self.bot = bot
     @commands.command(name='embed')
-    async def embed(self, ctx):
+    @commands.has_permissions(manage_messages=True)
+    async def embed(self, ctx,*, message):
         await ctx.message.delete()
         embed=discord.Embed()
         embed.add_field(name="Embed", value=f"{message}", inline=False)
         await ctx.send(embed=embed)
+    @commands.command(name='say')
+    @commands.has_permissions(manage_messages=True)
+    async def say(self, ctx,*, message=None):
+        await ctx.message.delete()
+        await ctx.send(message)
 def setup(bot):
     bot.add_cog(fun(bot))
