e4c6741755218235fe21132b0101433b8e950a08
OSP <76648940+osp54@users.noreply.github.com>
2021-08-01 13:45:38 +0300

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,1 +1,10 @@
 
+import discord
+from discord.ext import commands
+
+class moderation(commands.Cog, name="moderation"):
+    def __init__(self, bot):
+        self.bot = bot
+
+def setup(bot):
+    bot.add_cog(moderation(bot))
