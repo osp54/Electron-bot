79394218909c7d585870cf65ae869570b4d9cdb3
OSP <76648940+osp54@users.noreply.github.com>
2021-08-01 21:44:20 +0300

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 class Help(commands.Cog, name="help"):
     def __init__(self, bot):
         self.bot = bot
-    @commands.remove_command("help")
     @commands.command(name="help")
     async def help(self, context):
         """
