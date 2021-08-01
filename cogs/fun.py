938a1dd4a9f12def6a06701e53a4f7b7d54b7395
OSP <76648940+osp54@users.noreply.github.com>
2021-08-01 15:06:41 +0300

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,1 +1,14 @@
+import discord
+from discord.ext import commands
 
+class moderation(commands.Cog, name="fun"):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command(name='embed')
+    async def embed(self, ctx):
+        await ctx.message.delete()
+        embed=discord.Embed()
+        embed.add_field(name="Embed", value=f"{message}", inline=False)
+        await ctx.send(embed=embed)
+def setup(bot):
+    bot.add_cog(fun(bot))
