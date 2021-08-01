42e21228a8aaf928f43f68b41ca4f0ba3fe50cd4
OSP <76648940+osp54@users.noreply.github.com>
2021-08-01 13:49:01 +0300

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -5,6 +5,8 @@ from discord.ext import commands
 class moderation(commands.Cog, name="moderation"):
     def __init__(self, bot):
         self.bot = bot
-
+    @client.command()
+    async def test(ctx):
+        await ctx.send("test")
 def setup(bot):
     bot.add_cog(moderation(bot))
