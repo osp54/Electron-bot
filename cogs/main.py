d72524628f8f16343fbf743bd2c4d4f6c2d5f53f
OSP <76648940+osp54@users.noreply.github.com>
2021-08-02 14:12:05 +0300

    Create main.py

---
+++ cogs/main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class main(commands.Cog, name="main"):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command(name="poll")
+    async def poll(self, context, *, title):
+        """
+        Создайте опрос, в котором участники могут голосовать.
+        """
+        embed = discord.Embed(
+            title="Создан новый опрос!",
+            description=f"{title}",
+            color=0x42F56C
+        )
+        embed.set_footer(
+            text=f"Опрос создан: {context.message.author} • Жми на реакции!"
+        )
+        embed_message = await context.send(embed=embed)
+        await embed_message.add_reaction("👍")
+        await embed_message.add_reaction("👎")
+        await embed_message.add_reaction("🤷")
+def setup(bot):
+    bot.add_cog(main(bot))
