3d0f12e2a6edb4cc905e57f7a7b78001f507a532
OSP <76648940+osp54@users.noreply.github.com>
2021-08-02 12:33:29 +0300

    Command kick

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -16,5 +16,44 @@ class moderation(commands.Cog, name="moderation"):
             await ctx.send(embed=embed)
         except:
             await ctx.send("У вас недостаточно прав для этой команды.")
+    @commands.command(name='kick')
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, context, member: discord.Member, *, reason="Not specified"):
+        """
+        Кикнуть пользователя из сервера.
+        """
+        if member.guild_permissions.administrator:
+            embed = discord.Embed(
+                title="Ошибка",
+                description="У пользователя есть права администратора.",
+                color=0xE02B2B
+            )
+            await context.send(embed=embed)
+        else:
+            try:
+                await member.kick(reason=reason)
+                embed = discord.Embed(
+                    title="User Kicked!",
+                    description=f"**{member}** был кикнут модератором **{context.message.author}**!",
+                    color=0x42F56C
+                )
+                embed.add_field(
+                    name="Причина:",
+                    value=reason
+                )
+                await context.send(embed=embed)
+                try:
+                    await member.send(
+                        f"Вас кикнул **{context.message.author}**!\nПричина: {reason}"
+                    )
+                except:
+                    pass
+            except:
+                embed = discord.Embed(
+                    title="Ошибка!",
+                    description="Произошла ошибка при попытке кикнуть пользователя. Убедитесь, что моя роль выше роли пользователя, которого вы хотите кикнуть.",
+                    color=0xE02B2B
+                )
+                await context.message.channel.send(embed=embed)
 def setup(bot):
     bot.add_cog(moderation(bot))
