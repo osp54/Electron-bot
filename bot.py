017fbb1ac77856e512a217bb91a231537a8ec4a9
OSP <76648940+osp54@users.noreply.github.com>
2021-08-02 15:30:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,4 +41,5 @@ async def on_command_error(ctx, error):
         await ctx.send(embed = discord.Embed(title='Ошибка', description=f'**{ctx.author.name}**, Команда не найдена!', color=0xFF0000))
     if isinstance(error, commands.MissingPermissions):
         await ctx.send(embed = discord.Embed(title='Ошибка', description=f'**{ctx.author.name}**, У вас нет прав для использования этой команды.', color=0xFF0000)
+
 client.run('ODYxNTQxMjg3MTYxMTAyMzc2.YOLS2Q.Ja9sfwWISOUKDVdtcIsboP8JZ3k')
