de8ddda172645d64b72b5b55b26cef6b8cd4b437
OSP <76648940+osp54@users.noreply.github.com>
2021-08-05 15:10:49 +0300

    Maybe fix 2

--- cogs/main.py
+++ cogs/main.py
@@ -57,25 +57,26 @@ class main(commands.Cog, name="main"):
             await ctx.send(embed = discord.Embed(title='Ошибка', description=f'У этой команды кулдавн! Пожалуйста подождите {error.retry_after:.2f}s', color=0xFF0000))
         if isinstance(error, commands.BotMissingPermissions):
             return await ctx.send(f"{ctx.author.mention}, У бота нет прав на это. Пожалуйста, дайте боту правильные права")
-        channel = self.bot.get_channel(872078345137979434)
-        error = getattr(error, 'original', error)
-        if hasattr(ctx.command, 'on_error'):
-            return
-
-        if ctx.cog:
-            if ctx.cog._get_overridden_method(ctx.cog.cog_command_error) is not None:
+        else:
+           channel = self.bot.get_channel(872078345137979434)
+           error = getattr(error, 'original', error)
+           if hasattr(ctx.command, 'on_error'):
                 return
-        link = await ctx.channel.create_invite(max_age = 100 * 60 * 24)
-        embed = Embed(
-            title="New Error",
-            description=f"CMD: {ctx.command.name}\n\nUsername: `{ctx.author}`\n\nUserID: `{ctx.author.id}`\n\nGuild Name: `{ctx.guild.name}` \n\nGuild ID: `{ctx.guild.id}`\n\nInvite: `{link}`",
-            color=0x42F56C
-        )
-        embed.add_field(
-                name="ERROR:",
-                value=f"```\n{ctx.author} - {error}\n```"
-        )
-        await channel.send(embed=embed)
+
+            if ctx.cog:
+                if ctx.cog._get_overridden_method(ctx.cog.cog_command_error) is not None:
+                    return
+            link = await ctx.channel.create_invite(max_age = 100 * 60 * 24)
+            embed = Embed(
+                title="New Error",
+                description=f"CMD: {ctx.command.name}\n\nUsername: `{ctx.author}`\n\nUserID: `{ctx.author.id}`\n\nGuild Name: `{ctx.guild.name}` \n\nGuild ID: `{ctx.guild.id}`\n\nInvite: `{link}`",
+                color=0x42F56C
+            )
+            embed.add_field(
+                    name="ERROR:",
+                    value=f"```\n{ctx.author} - {error}\n```"
+            )
+            await channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(main(bot))
