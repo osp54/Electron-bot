3daf893329e91c9add338d1857a785ec3485e058
OSP <76648940+osp54@users.noreply.github.com>
2021-08-11 17:19:23 +0300

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,27 +1,25 @@
 import discord
 from discord.ext import commands
-
+import os
+import sys
+import json
+with open("owners.json") as file:
+    owners = json.load(file)
 class owner(commands.Cog, name="owner"):
     def __init__(self, bot):
         self.bot = bot
     @commands.command(name='embed')
-    @commands.has_permissions(manage_messages=True)
-    async def embed(self, ctx,*, message="No Arguments"):
-        """
-        Отправить эмбед сообщение
-        """
-        await ctx.message.delete()
-        embed=discord.Embed()
-        embed.add_field(name="Embed", value=f"{message}", inline=False)
-        await ctx.send(embed=embed)
+    async def embed(self, ctx,*, message):
+        if ctx.message.author.id in owners["owners"]:
+            await ctx.message.delete()
+            embed=discord.Embed()
+            embed.add_field(value=f"{message}", inline=False)
+            await ctx.send(embed=embed)
     @commands.command(name='say')
-    @commands.has_permissions(manage_messages=True)
     async def say(self, ctx,*, message=None):
-        """
-        Отправить сообщение от имени бота
-        """
-        await ctx.message.delete()
-        await ctx.send(message)
+        if ctx.message.author.id in owners["owners"]:
+            await ctx.message.delete()
+            await ctx.send(message)
 
 def setup(bot):
     bot.add_cog(owner(bot))
