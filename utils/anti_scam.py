bab6fa32174231c9f6975774bca1ef1cbd79b062
OSP <76648940+osp54@users.noreply.github.com>
2021-08-11 12:26:13 +0300

    Update anti_scam.py

--- utils/anti_scam.py
+++ utils/anti_scam.py
@@ -4,7 +4,9 @@ from discord.ext import commands
 class anti_scam(commands.Cog, name="anti_scam"):
     def __init__(self, bot):
         self.bot = bot
-    
 
+    @commands.Cog.listener()
+    async def on_message(self, ctx, message):
+        #TODO
 def setup(bot):
     bot.add_cog(anti_scam(bot))
