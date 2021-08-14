f958a41edb3c3d070a2eda1178f5c02f124602b5
OSP <76648940+osp54@users.noreply.github.com>
2021-08-14 18:08:09 +0300

    Owners

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ from config import settings
 from discord.ext import tasks,commands
 
 client = discord.Client()
-client = commands.Bot(command_prefix = settings['prefix'])
+owners = [580631356485402639, 530103444946812929]
+client = commands.Bot(command_prefix = settings['prefix'], owner_ids = set(owners))
 client.remove_command('help')
 #когсы комманд
 if __name__ == "__main__":
