8f0c3de31cb538dd63d65092c848e30ea6f3d9e0
OSP <76648940+osp54@users.noreply.github.com>
2021-08-14 18:16:23 +0300

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -5,44 +5,42 @@ import sys
 import json
 import contextlib
 import io
-with open("owners.json") as file:
-    owners = json.load(file)
 class owner(commands.Cog, name="owner"):
     def __init__(self, bot):
         self.bot = bot
     def restart_bot(): 
         os.execv(sys.executable, ['python'] + sys.argv)
     @commands.command(name="eval")
+    @commands.is_owner()
     async def eval(self, ctx, *, code):
-        if ctx.message.author.id in owners["owners"]:
-            str_obj = io.StringIO()
-            try:
-                with contextlib.redirect_stdout(str_obj):
-                    exec(code)
-            except Exception as e:
-                return await ctx.send(f"```{e.__class__.__name__}: {e}```")
-            await ctx.send(f'```{str_obj.getvalue()}```')
+        str_obj = io.StringIO()
+        try:
+            with contextlib.redirect_stdout(str_obj):
+                exec(code)
+        except Exception as e:
+            return await ctx.send(f"```{e.__class__.__name__}: {e}```")
+        await ctx.send(f'```{str_obj.getvalue()}```')
     @commands.command(name="restart")
+    @commands.is_owner()
     async def restart(self,ctx):
-        if ctx.message.author.id in owners["owners"]:
-            embed = discord.Embed(
-                title="Restarting...",
-                description="Бот перезапускается...",
-                color=0x42F56C
-            )
-            await ctx.send(embed=embed)
-            restart_bot()
+        embed = discord.Embed(
+            title="Restarting...",
+            description="Бот перезапускается...",
+            color=0x42F56C
+        )
+        await ctx.send(embed=embed)
+        restart_bot()
     @commands.command(name='embed')
+    @commands.is_owner()
     async def embed(self, ctx,*, message):
-        if ctx.message.author.id in owners["owners"]:
-            await ctx.message.delete()
-            embed=discord.Embed(description=message, color=0xFF0000)
-            await ctx.send(embed=embed)
+        await ctx.message.delete()
+        embed=discord.Embed(description=message, color=0xFF0000)
+        await ctx.send(embed=embed)
     @commands.command(name='say')
+    @commands.is_owner()
     async def say(self, ctx,*, message=None):
-        if ctx.message.author.id in owners["owners"]:
-            await ctx.message.delete()
-            await ctx.send(message)
+        await ctx.message.delete()
+        await ctx.send(message)
 
 def setup(bot):
     bot.add_cog(owner(bot))
