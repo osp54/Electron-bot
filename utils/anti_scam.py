043e9da8f5da0a41db5132f069d60b575c6c1a53
thedimas3007 <dima.orischenko@gmail.com>
2021-08-15 22:28:49 +0300

    more scam links

--- utils/anti_scam.py
+++ utils/anti_scam.py
@@ -1,14 +1,17 @@
 import discord
 from discord.ext import commands
 
+scam_links = ["steamcommunity.link", "discorcl.link"]
+
 class anti_scam(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message):
-         if "steamcommunity.link" in message.content:
-             await message.delete()
-         #TODO: больше скам ссылок
+        for link in scam_links:
+            if link in message.content:
+                await message.delete()
+        #TODO: больше скам ссылок
 def setup(bot):
     bot.add_cog(anti_scam(bot))
