bb0fb53c49eb58ac4425446feb3222515ed5f538
OSP <76648940+osp54@users.noreply.github.com>
2021-08-15 18:06:49 +0300

    Смена префиксов 2

--- utils/events.py
+++ utils/events.py
@@ -1,9 +1,29 @@
 import discord
+import json
 from discord.ext import commands
 
 class events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        with open("prefixes.json", "r") as f:
+            prefixes = json.load(f)
+
+        prefixes[str(guild.id)] = "m!"
+        with open("prefixes.json", "w") as f:
+            json.dump(prefixes, f, indent=4)
+
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        with open("prefixes.json", "r") as f:
+            prefixes = json.load(f)
+
+        prefixes.pop(str(guild.id))
+        with open("prefixes.json", "w") as f:
+            json.dump(prefixes, f, indent=4)
+
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
