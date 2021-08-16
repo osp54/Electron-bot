5d94728fc3b765ca53fd6f20a71007e03ad0093c
OSP <76648940+osp54@users.noreply.github.com>
2021-08-16 12:17:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 settings = {
-    'token': 'ODYxNTQxMjg3MTYxMTAyMzc2.YOLS2Q.Ja9sfwWISOUKDVdtcIsboP8JZ3k',
-    'prefix': '$'
+    'token': 'ODYxNTQxMjg3MTYxMTAyMzc2.YOLS2Q.Ja9sfwWISOUKDVdtcIsboP8JZ3k'
 }
