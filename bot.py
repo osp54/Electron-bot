74e33f80174182a34f9b9a887b70fb5d05bccf41
OSP <76648940+osp54@users.noreply.github.com>
2021-08-21 19:21:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ def info(desc):
     print(f"{Fore.BLUE}[{Fore.RESET}{now.day}:{now.hour}:{now.minute}:{now.second}{Fore.BLUE}] " + Fore.BLUE + f"[I] {Fore.RESET}" + desc)
 def error(desc):
     now = pendulum.now('Europe/Moscow')
-    print(f"{Fore.BLUE}[{Fore.RESET}{now.day}:{now.hour}:{now.minute}:{now.second}{Fore.BLUE}] " + Fore.RED + f"[E] {Fore.RESET}" + desc)
+    print(f"{Fore.RED}[{Fore.RESET}{now.day}:{now.hour}:{now.minute}:{now.second}{Fore.RED}] " + Fore.RED + f"[E] {Fore.RESET}" + desc)
 
 #загрузить все расширения из папки
 def load_extensions(dir):
