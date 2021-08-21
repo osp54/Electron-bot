7066f9531ae978f346a6333824fccb7e06dad2ff
OSP <76648940+osp54@users.noreply.github.com>
2021-08-21 19:51:49 +0300

    Load extension command

--- cogs/owner.py
+++ cogs/owner.py
@@ -2,10 +2,19 @@ import discord
 from discord.ext import commands
 import os
 import sys
+from bot import info, error
 class owner(commands.Cog, name="owner"):
     def __init__(self, bot):
         self.bot = bot
-
+    @commands.command(name='load')
+    async def load(self, ctx, dir, cog)
+        try:
+            self.bot.load_extension(f"{dir[2:]}.{cog}")
+            info(f"Loaded extension {cog}")
+            await ctx.send("Готово!")
+        except Exception as e:
+            exception = f"{type(e).__name__}: {e}"
+            error(f"Failed to load extension {cog}\n{exception}")
     @commands.command(name='embed')
     @commands.is_owner()
     async def embed(self, ctx,*, message):
