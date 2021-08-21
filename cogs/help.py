4efe37f02d54592348a2da3f0efa6bcec3289b19
OSP <76648940+osp54@users.noreply.github.com>
2021-08-21 17:33:44 +0300

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -20,6 +20,7 @@ class help(commands.Cog, name="help"):
             command_description = [command.help for command in commands]
             help_text = '\n'.join(f'{n} - {h}' for n, h in zip(command_list, command_description))
             embed.add_field(name=i.capitalize(), value=f'```\n{help_text}\n```', inline=False)
+            embed.set_footer(text='ElectronBot.tk | Все права защищены')
         await context.send(embed=embed)
 
 def setup(bot):
