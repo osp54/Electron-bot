143411276ab783edd6b28eccfb6b8f65626cd88a
OSP <76648940+osp54@users.noreply.github.com>
2021-08-24 14:25:45 +0300

    Help command group

--- cogs/help.py
+++ cogs/help.py
@@ -5,14 +5,30 @@ from discord.ext.commands import cooldown, BucketType
 class help(commands.Cog, name="help"):
     def __init__(self, bot):
         self.bot = bot
-    @commands.command(name="help", aliases=['хелп', 'помощь'])
+    @commands.group(name="help", aliases=['хелп', 'помощь'])
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def help(self, context):
         """
         Список всех команд
         """
+        if ctx.invoked_subcommand is None:
+            embed = discord.Embed(title="Help", description=f"Префикс: {context.prefix}", color=0x42F56C)
+            cogs = ("Main", "Moderation", "Music")
+            for i in cogs:
+                cog = self.bot.get_cog(i.lower())
+                commands = cog.get_commands()
+                command_list = [command.name for command in commands]
+                command_description = [command.help for command in commands]
+                help_text = '\n'.join(f'{n} - {h}' for n, h in zip(command_list, command_description))
+                embed.add_field(name=i.capitalize(), value=f'```\n{help_text}\n```', inline=False)
+                embed.set_footer(text='ElectronBot.tk | Все права защищены')
+                embed.set_author(name=context.message.author, icon_url=context.message.author.avatar.url)
+            await context.send(embed=embed)
+     @help.command()
+     @commands.cooldown(1, 2, commands.BucketType.user)
+     async def main(self, ctx)
         embed = discord.Embed(title="Help", description=f"Префикс: {context.prefix}", color=0x42F56C)
-        cogs = ("Main", "Moderation", "Music")
+        cogs = ("Main")
         for i in cogs:
             cog = self.bot.get_cog(i.lower())
             commands = cog.get_commands()
@@ -22,7 +38,36 @@ class help(commands.Cog, name="help"):
             embed.add_field(name=i.capitalize(), value=f'```\n{help_text}\n```', inline=False)
             embed.set_footer(text='ElectronBot.tk | Все права защищены')
             embed.set_author(name=context.message.author, icon_url=context.message.author.avatar.url)
-        await context.send(embed=embed)
-
+       await context.send(embed=embed)
+     @help.command()
+     @commands.cooldown(1, 2, commands.BucketType.user)
+     async def moderation(self, ctx)
+        embed = discord.Embed(title="Help", description=f"Префикс: {context.prefix}", color=0x42F56C)
+        cogs = ("Moderation")
+        for i in cogs:
+            cog = self.bot.get_cog(i.lower())
+            commands = cog.get_commands()
+            command_list = [command.name for command in commands]
+            command_description = [command.help for command in commands]
+            help_text = '\n'.join(f'{n} - {h}' for n, h in zip(command_list, command_description))
+            embed.add_field(name=i.capitalize(), value=f'```\n{help_text}\n```', inline=False)
+            embed.set_footer(text='ElectronBot.tk | Все права защищены')
+            embed.set_author(name=context.message.author, icon_url=context.message.author.avatar.url)
+       await context.send(embed=embed)
+     @help.command()
+     @commands.cooldown(1, 2, commands.BucketType.user)
+     async def music(self, ctx)
+        embed = discord.Embed(title="Help", description=f"Префикс: {context.prefix}", color=0x42F56C)
+        cogs = ("Music")
+        for i in cogs:
+            cog = self.bot.get_cog(i.lower())
+            commands = cog.get_commands()
+            command_list = [command.name for command in commands]
+            command_description = [command.help for command in commands]
+            help_text = '\n'.join(f'{n} - {h}' for n, h in zip(command_list, command_description))
+            embed.add_field(name=i.capitalize(), value=f'```\n{help_text}\n```', inline=False)
+            embed.set_footer(text='ElectronBot.tk | Все права защищены')
+            embed.set_author(name=context.message.author, icon_url=context.message.author.avatar.url)
+       await context.send(embed=embed)
 def setup(bot):
     bot.add_cog(help(bot))
