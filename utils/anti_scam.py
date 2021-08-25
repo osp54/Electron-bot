ffec4627597c8878e881b0ba0cd133ff6f93737b
OSP <76648940+osp54@users.noreply.github.com>
2021-08-25 12:46:33 +0300

    More scam links

--- utils/anti_scam.py
+++ utils/anti_scam.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-scam_links = ["steamcommunity.link", "discorcl.link", "steamcommunity.com.ru", "steamcommunnity.ru", "steamccommunity.com.ru", "steamcommuunity.com.ru"]
+scam_links = ["steamcommunity.link", "discorcl.link", "steamcommunity.com.ru", "steamcommunnity.ru", "steamccommunity.com.ru", "steamcommuunity.com.ru", "surl.li", "stearncornmrunity.ru.com", "fustcup.ru", "steamcommrunity.com", "brapo.space", "steamcommunutiy.com"]
 
 class anti_scam(commands.Cog):
     def __init__(self, bot):
