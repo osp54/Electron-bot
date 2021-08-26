471091843c54e70efa8dd43a071eeedb8ef12a10
OSP <76648940+osp54@users.noreply.github.com>
2021-08-26 21:24:26 +0300

    Reactions, and moving commands

--- cogs/music.py
+++ cogs/music.py
@@ -21,25 +21,6 @@ class music(commands.Cog, name="music"):
     async def on_play(self, ctx, player):
         await ctx.send(f"Играет `{player}`\n{player.data['webpage_url']}")
 
-    @commands.command(aliases=['выйти'])
-    @commands.cooldown(1, 2, commands.BucketType.user)
-    async def leave(self, ctx):
-        """
-        Выйти из голосового канала.
-        """
-        if await self.MusicManager.leave(ctx):
-            await ctx.send("Вышел из голосового канала.")
-
-
-    @commands.command(aliases=['си', 'сейчас играет', 'now playing'])
-    @commands.cooldown(1, 2, commands.BucketType.user)
-    async def np(self, ctx):
-        """
-        Сейчас играет.
-        """
-        await ctx.send(f"Сейчас играет: `{await self.MusicManager.now_playing(ctx)}`")
-
-
     @commands.command(aliases=['присоед.'])
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def join(self, ctx):
@@ -48,7 +29,17 @@ class music(commands.Cog, name="music"):
         """
         if await self.MusicManager.join(ctx):
             await ctx.send("Присоединился к голосовому каналу.")
+            await ctx.message.add_reaction('✅')
 
+    @commands.command(aliases=['выйти'])
+    @commands.cooldown(1, 2, commands.BucketType.user)
+    async def leave(self, ctx):
+        """
+        Выйти из голосового канала.
+        """
+        if await self.MusicManager.leave(ctx):
+            await ctx.send("Вышел из голосового канала.")
+            await ctx.message.add_reaction('✅')
 
     @commands.command(aliases=['проиграть'])
     @commands.cooldown(1, 2, commands.BucketType.user)
@@ -62,11 +53,29 @@ class music(commands.Cog, name="music"):
         player = await self.MusicManager.create_player(query)
         if player:
             await self.MusicManager.queue_add(player=player, ctx=ctx)
-
+            await ctx.message.add_reaction('✅')
             if not await self.MusicManager.play(ctx):
                 await ctx.send(f"Добавлено в очередь")
+                await ctx.message.add_reaction('✅')
         else:
             await ctx.send("Запрос не найден.")
+
+    @commands.command(aliases=['скип'])
+    @commands.cooldown(1, 2, commands.BucketType.user)
+    async def skip(self, ctx, index: int = None):
+        """
+        Пропустить текущую песню.
+        """
+        await self.MusicManager.skip(ctx, index)
+        await ctx.message.add_reaction('✅')
+
+    @commands.command(aliases=['си', 'сейчас играет', 'now playing'])
+    @commands.cooldown(1, 2, commands.BucketType.user)
+    async def np(self, ctx):
+        """
+        Сейчас играет.
+        """
+        await ctx.send(f"Сейчас играет: `{await self.MusicManager.now_playing(ctx)}`")
 
     @commands.command()
     @commands.cooldown(1, 2, commands.BucketType.user)
@@ -75,20 +84,20 @@ class music(commands.Cog, name="music"):
         Сделать больше/меньше громкость звука
         """
         await self.MusicManager.volume(ctx, volume)
-
+        await ctx.message.add_reaction('✅')
 
     @commands.command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def loop(self, ctx):
         is_loop = await self.MusicManager.loop(ctx)
         await ctx.send(f"Looping toggled to `{is_loop}`")
-
+        await ctx.message.add_reaction('✅')
     @commands.command()
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def queueloop(self, ctx):
         is_loop = await self.MusicManager.queueloop(ctx)
         await ctx.send(f"Queue looping toggled to `{is_loop}`")
-
+        await ctx.message.add_reaction('✅')
     @commands.command(aliases=['история'])
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def history(self, ctx):
@@ -103,14 +112,6 @@ class music(commands.Cog, name="music"):
 
         page_manager = PageManager(ctx, embeds, public=True)
         await page_manager.run()
-
-    @commands.command(aliases=['скип'])
-    @commands.cooldown(1, 2, commands.BucketType.user)
-    async def skip(self, ctx, index: int = None):
-        """
-        Пропустить текущую песню.
-        """
-        await self.MusicManager.skip(ctx, index)
 
     @commands.command(aliases=['очередь'])
     @commands.cooldown(1, 2, commands.BucketType.user)
