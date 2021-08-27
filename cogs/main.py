f42666c0aa7b21ef131c94b7361394464619bcc0
OSP <76648940+osp54@users.noreply.github.com>
2021-08-28 01:25:35 +0300

    Info commands

--- cogs/main.py
+++ cogs/main.py
@@ -1,5 +1,7 @@
 import discord
 import json
+import datetime
+import humanize
 from discord.ext import commands
 from discord.ext.commands import cooldown, BucketType
 
@@ -55,26 +57,83 @@ class main(commands.Cog, name="main"):
         )
         embed.set_image(url=member.avatar.url)
         await ctx.send(embed=embed)
-    @commands.command()
+    @commands.command(aliases=["эмодзи_инфо", "emoteinfo"])
     @commands.cooldown(1, 2, commands.BucketType.user)
-    async def emoji(self, ctx, emoji: discord.Emoji = None):
+    async def emoji(self, ctx, emoji: discord.Emoji):
         """
-        Получить изображение эмодзи.
+        Эмодзи.
         """
-        if not emoji:
-            embed = discord.Embed(
-                title="Ошибка",
-                description="Введите эмодзи!",
-                color=0xE02B2B
-            )
-            await ctx.send(embed=embed)
-            return
-        eembed = discord.Embed(
-            title=f"Эмодзи {emoji.name}",
-            color=0x42F56C
+        embed = discord.Embed(title=f"Эмодзи {emoji.name}", description="\\" + str(emoji))
+        embed.set_thumbnail(url=emoji.url)
+        embed.set_image(url=emoji.url)
+        embed.add_field(name="ID", value=emoji.id)
+        if emoji.user:
+            embed.add_field(name="Кем добавлено", value=emoji.user)
+        embed.add_field(name="Server", value=emoji.guild)
+        embed.add_field(
+            name="Создано",
+            value=f'{discord.utils.format_dt(emoji.created_at, "F")} ({discord.utils.format_dt(emoji.created_at, "R")})',
         )
-        eembed.set_image(url=emoji.url)
-        await ctx.send(embed=eembed)
+        embed.add_field(name="URL", value=f"[Здесь]({emoji.url})")
+        await ctx.send(embed=embed)
+    @commands.command(aliases=["guildinfo", "сервер"])
+    @commands.cooldown(1, 2, commands.BucketType.user)
+    async def serverinfo(self, ctx):
+        """Информация об этом сервере."""
+        guild = ctx.guild
+        guild_owner = self.bot.get_user(guild.owner_id)
+
+        features = "\n".join(format_name(f) for f in guild.features)
+
+        embed = discord.Embed(
+            title=f"Сервер {guild.name}",
+            description=(
+                f"Имя: {guild.name}\n"
+                f"Создан: {discord.utils.format_dt(guild.created_at, 'F')} ({discord.utils.format_dt(guild.created_at, 'R')})"
+                f"ID: {guild.id}\nOwner: {guild_owner}\n"
+                f"Icon URL: [click here]({guild.icon.url})\n"
+                f"Регион: {str(guild.region)}\n"
+                f"Уровень верификации: {str(guild.verification_level)}\n"
+                f"Участников: {len(guild.members)}\n"
+                f"Уровень бустов: {guild.premium_tier}\n"
+                f"Бустов: {guild.premium_subscription_count}\n"
+                f"Бустеров: {len(guild.premium_subscribers)}\n"
+                f"Каналов: {len(guild.channels)}\n"
+                f"Текстовых каналов: {len(guild.text_channels)}\n"
+                f"Голосовых каналов: {len(guild.voice_channels)}\n"
+                f"Категорий: {len(guild.categories)}\n"
+                f"Ролей: {len(guild.roles)}\n"
+                f"Эмодзи: {len(guild.emojis)}/{guild.emoji_limit}\n"
+                f"Лимит отправки: {round(guild.filesize_limit / 1048576)} MB\n"
+                f"**Features:** {features}"
+            ),
+        )
+        embed.set_thumbnail(url=guild.icon.url)
+        await ctx.send(embed=embed)
+    @commands.command(aliases=['ci', 'канал'])
+    async def channelinfo(self, ctx, channel: discord.TextChannel = None):
+        """
+        Информация о текущем/другом канале.
+        """
+        channel = channel or ctx.channel
+        embed = discord.Embed(title=f"Информация об {channel.name}, color=0x42F56C)
+        if channel.topic:
+            embed.add_field(name="Описание", value=channel.topic, inline=False)
+        embed.add_field(
+            name="Создано",
+            value=f'{discord.utils.format_dt(channel.created_at, "F")}  ({discord.utils.format_dt(channel.created_at, "R")})',
+        )
+        embed.add_field(name="ID", value=channel.id)
+        embed.add_field(name="Тип", value=channel.type)
+        embed.add_field(name="Позиция", value=f"{channel.position}/{len(ctx.guild.text_channels)}")
+        embed.add_field(name="Категория", value=channel.category.name)
+        if channel.slowmode_delay:
+            embed.add_field(
+                name="Слоумод",
+                value=f"{channel.slowmode_delay} секунд ({humanize.naturaldelta(datetime.timedelta(seconds=int(channel.slowmode_delay)))})",
+            )
+        
+        await ctx.send(embed=embed)
     @commands.command(name="poll", aliases=['опрос'])
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def poll(self, ctx, *, title):
