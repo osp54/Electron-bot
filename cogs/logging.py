a8a1b8ebbda49a1f78c5a2b10d478d76bda19394
OSP <76648940+osp54@users.noreply.github.com>
2021-08-30 00:41:38 +0300

    Update logging.py

--- cogs/logging.py
+++ cogs/logging.py
@@ -1,14 +1,15 @@
 import nextcord
 from nextcord.ext import commands
+from colorama import init, Fore, Back, Style
 from functions import error, info
 class owner(commands.Cog, name="owner"):
     def __init__(self, bot):
         self.bot = bot
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        info(f"Added new guild! Name: {guild.name}, members: {guild.member_count}.")
+        info(f"Added new guild! Name: {Fore.CYAN}{guild.name}{Fore.RESET}, members: {Fore.CYAN}{guild.member_count}")
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        info(f"Removed guild. Name: {guild.name}, members: {guild.member_count}.")
+        info(f"Removed guild. Name: {Fore.CYAN}{guild.name}{Fore.RESET}, members: {Fore.CYAN}{guild.member_count}")
 def setup(bot):
     bot.add_cog(owner(bot))
