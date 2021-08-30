afb3d0eccbb3a5a7eb376c29f1c2d74982001036
OSP <76648940+osp54@users.noreply.github.com>
2021-08-30 19:10:15 +0300

    Update status.py

--- cogs/status.py
+++ cogs/status.py
@@ -2,7 +2,7 @@ import nextcord as discord
 from nextcord.ext import commands, tasks
 import psutil
 import os
-import time
+import pendulum
 
 tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
 
@@ -12,11 +12,12 @@ class status(commands.Cog):
 
     @tasks.loop(seconds=30)
     async def sstatus(self):
+        now = pendulum.now('Europe/Moscow')
         channel = self.bot.get_channel(881924597472178176)
         msg = await channel.fetch_message(881924714073845760)
         emb = discord.Embed(
             title = "Статистика",
-            description = f"Статистика нагрузки на хостинг бота.\nПоследнее обновление статуса: ",
+            description = f"Статистика нагрузки на хостинг бота.\nПоследнее обновление статуса: {discord.utils.format_dt(now, "F")}",
             color = 0x00A725
         )
         emb.add_field( name = "CPU", value = f"Нагрузка на процессор: {psutil.cpu_percent()}% ", inline = False)
