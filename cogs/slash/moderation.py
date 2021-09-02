61e443d7a33ef76e25175d0cf504ad22f2849db7
OSP <76648940+osp54@users.noreply.github.com>
2021-09-02 21:45:18 +0300

    Update moderation.py

--- cogs/slash/moderation.py
+++ cogs/slash/moderation.py
@@ -1,1 +1,11 @@
 
+import nextcord
+from nextcord.ext import commands
+from nextcord.ext.commands import cooldown, BucketType
+
+class slashmoderation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+def setup(bot):
+    bot.add_cog(slashmoderation(bot))
