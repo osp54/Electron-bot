75bfe672bb7439af88ef5af0e9e4905ff407dd96
OSP <76648940+osp54@users.noreply.github.com>
2021-09-04 19:57:03 +0300

    Test

--- cogs/darkdustry.py
+++ cogs/darkdustry.py
@@ -7,9 +7,13 @@ class darkdustry(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.server = pydustry.Server('localhost', server_port = 6567, socketinput_port = 7777)
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def restart(self, ctx):
+    @commands.group()
+    @commands.has_role(869879808216150057)
+    async def server(self, ctx):
+        pass
+    @server.command()
+    @commands.has_role(869879808216150057)
+    async def test(self, ctx):
         self.server.send_command('rr')
 def setup(bot):
     bot.add_cog(darkdustry(bot))
