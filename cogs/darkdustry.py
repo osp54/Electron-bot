adf8b70ad2dc317f8edb039e99f7c56d25fbfb19
OSP <76648940+osp54@users.noreply.github.com>
2021-09-04 19:18:49 +0300

    Update darkdustry.py

--- cogs/darkdustry.py
+++ cogs/darkdustry.py
@@ -6,6 +6,10 @@ from nextcord.ext.commands import cooldown, BucketType
 class darkdustry(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+        self.server = pydustry.Server('mindurka.tk', server_port = 6567, socketinput_port = 6859)
+    @commands.command()
+    @commands.has_permission(administrator=True)
+    async def restart(self, ctx):
+        self.server.send_command('rr')
 def setup(bot):
     bot.add_cog(darkdustry(bot))
