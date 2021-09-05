c104bc9234a59fdc338350319a36ec0bf63faf83
OSP <76648940+osp54@users.noreply.github.com>
2021-09-05 11:08:13 +0300

    Update darkdustry.py

--- cogs/darkdustry.py
+++ cogs/darkdustry.py
@@ -9,7 +9,8 @@ class darkdustry(commands.Cog):
         self.server = None
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.endswith('#0000'):
+        author = message.author
+        if author.endswith('#0000'):
             return
         channel = self.bot.get_channel(871410960986939473)
         self.server = pydustry.Server('localhost', server_port = 6567, socketinput_port = 7777)
