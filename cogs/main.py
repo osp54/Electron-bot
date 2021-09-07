4873da1f172a00b034aaabc75b9bf8d9c154ea36
OSP <76648940+osp54@users.noreply.github.com>
2021-09-07 18:03:16 +0300

    Update main.py

--- cogs/main.py
+++ cogs/main.py
@@ -15,7 +15,7 @@ class main(commands.Cog, name="main"):
     @commands.command()
     async def test(self, ctx):
         bundle = ConfigParser()
-        bundle.read(f"{get_lang(client=self.bot, message=ctx.message)}.ini")
+        bundle.read(f"{get_lang(self.bot, ctx.message)}.ini")
         await ctx.send(bundle.get("Bundle", "embedTitle"))
     @commands.command(
         name = "setprefix",
