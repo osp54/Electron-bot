50fa51d10976340344804e82ca273fb3c5760df4
OSP <76648940+osp54@users.noreply.github.com>
2021-09-12 08:54:49 +0300

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -1,5 +1,5 @@
 import nextcord
-from utils.misc import get_lang
+from utils.misc import get_lang, get_prefix
 from configparser import ConfigParser
 from nextcord.ext import commands
 from nextcord.ext.commands import cooldown, BucketType
@@ -17,15 +17,16 @@ class help(commands.Cog, name="help"):
         self.b.read(f"locales/{get_lang(self.bot, ctx.message)}.ini")
         if command is None:
             embed = nextcord.Embed(title="help", color=0x2B95FF)
+            prefix = get_prefix(self.bot, ctx.message)
             text = ""
             text2 = ""
             maincog = self.bot.get_cog("main").get_commands()
             modcog = self.bot.get_cog("moderation").get_commands()
             for cmd in maincog:
-                text += cmd.name + " - " + self.b.get("Bundle", f"{cmd}.description") + "\n"
+                text += prefix + cmd.name + " - " + self.b.get("Bundle", f"{cmd}.description") + "\n"
             embed.add_field(name=self.b.get("Bundle", "embed.help.main"), value=text)
             for cmd in modcog:
-                text2 += cmd.name + " - " + self.b.get("Bundle", f"{cmd}.description") + "\n"
+                text2 += prefix + cmd.name + " - " + self.b.get("Bundle", f"{cmd}.description") + "\n"
             embed.add_field(name=self.b.get("Bundle", "embed.help.moderation"), value=text2)
             await ctx.send(embed=embed) 
         if command is not None:
