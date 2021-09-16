b83a5219fb02847871cdce171d5e5c02b8423dce
OSP <76648940+osp54@users.noreply.github.com>
2021-09-16 19:56:08 +0300

    utils.json - Функции для добавления гильдии в чс

---
+++ utils/json.py
@@ -0,0 +1,19 @@
+import json
+
+
+def add_guild_to_BL(user_id: int):
+    with open("blackguilds.json", "r+") as file:
+        file_data = json.load(file)
+        file_data["ids"].append(user_id)
+    with open("blackguilds.json", "w") as file:
+        file.seek(0)
+        json.dump(file_data, file, indent=4)
+
+
+def rm_guild_from_BL(user_id: int):
+    with open("blackguilds.json", "r") as file:
+        file_data = json.load(file)
+        file_data["ids"].remove(user_id)
+    with open("blackguilds.json", "w") as file:
+        file.seek(0)
+        json.dump(file_data, file, indent=4)
