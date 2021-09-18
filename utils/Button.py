a7d0fa79a848b789ec6529682b620f528b80a2eb
OSP <76648940+osp54@users.noreply.github.com>
2021-09-18 12:44:39 +0300

    Buttons

--- utils/Button.py
+++ utils/Button.py
@@ -1,1 +1,66 @@
+import nextcord
 
+class ConfirmButton(nextcord.ui.View):
+    def __init__(self, user):
+        super().__init__()
+        self.value = None
+        self.user = user
+
+    @nextcord.ui.button(emoji='✅', style=nextcord.ButtonStyle.green)
+    async def confirm(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        if interaction.user.id != self.user.id:
+            return await interaction.response.send_message('Ты не автор команды!', ephemeral=True)
+        await interaction.response.send_message('Готово.', ephemeral=True)
+        self.value = True
+        self.stop()
+
+    @nextcord.ui.button(emoji='❌', style=nextcord.ButtonStyle.gray)
+    async def cancel(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        if interaction.user.id != self.user.id:
+            return await interaction.response.send_message('Ты не автор команды!', ephemeral=True)
+        await interaction.response.send_message('Отклонено.', ephemeral=True)
+        self.value = False
+        self.stop()
+
+class ConfirmButton(nextcord.ui.View):
+    def __init__(self, user):
+        super().__init__()
+        self.value = None
+        self.user = user
+    @nextcord.ui.button(emoji='❌', style=nextcord.ButtonStyle.green)
+    async def confirm(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        if interaction.user.id != self.user.id:
+            return await interaction.response.send_message('Ты не автор команды!', ephemeral=True)
+        await interaction.response.send_message('Готово.', ephemeral=True)
+        self.value = True
+        self.stop()
+
+    @nextcord.ui.button(emoji='✅', style=nextcord.ButtonStyle.grey)
+    async def cancel(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        if interaction.user.id != self.user.id:
+            return await interaction.response.send_message('Ты не автор команды!', ephemeral=True)
+        await interaction.response.send_message('Отклонено.', ephemeral=True)
+        self.value = False
+        self.stop()
+
+class SetLangButton(nextcord.ui.View):
+    def __init__(self, user):
+        super().__init__()
+        self.value = None
+        self.user = user
+
+    @nextcord.ui.button(label="English", style=nextcord.ButtonStyle.green)
+    async def english(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        if interaction.user.id != self.user.id:
+            return
+        await interaction.response.send_message('The language of my messages has been successfully set to English!')
+        self.value = True
+        self.stop()
+
+    @nextcord.ui.button(label="Русский", style=nextcord.ButtonStyle.gray)
+    async def russian(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        if interaction.user.id != self.user.id:
+            return
+        await interaction.response.send_message('Язык моих сообщений успешно установлен на Русский!')
+        self.value = False
+        self.stop()
