4aae27e331749b6e49adea511a4ac91ddb234945
OSP <76648940+osp54@users.noreply.github.com>
2021-09-18 18:07:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,6 +69,7 @@ async def on_ready():
 
 if __name__ == "__main__":
     load_extensions("./cogs") #когсы командc
+    client.load_extension("jishaku")
 try:
     client.run(os.environ['TOKEN'])
 except KeyboardInterrupt as k:
