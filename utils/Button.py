f49581e2a0f42c8fc01470ed7535dd08966dedc2
OSP <76648940+osp54@users.noreply.github.com>
2021-09-18 12:47:11 +0300

    Update Button.py

--- utils/Button.py
+++ utils/Button.py
@@ -22,27 +22,6 @@ class ConfirmButton(nextcord.ui.View):
         self.value = False
         self.stop()
 
-class ConfirmButton(nextcord.ui.View):
-    def __init__(self, user):
-        super().__init__()
-        self.value = None
-        self.user = user
-    @nextcord.ui.button(emoji='❌', style=nextcord.ButtonStyle.green)
-    async def confirm(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        if interaction.user.id != self.user.id:
-            return await interaction.response.send_message('Ты не автор команды!', ephemeral=True)
-        await interaction.response.send_message('Готово.', ephemeral=True)
-        self.value = True
-        self.stop()
-
-    @nextcord.ui.button(emoji='✅', style=nextcord.ButtonStyle.grey)
-    async def cancel(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        if interaction.user.id != self.user.id:
-            return await interaction.response.send_message('Ты не автор команды!', ephemeral=True)
-        await interaction.response.send_message('Отклонено.', ephemeral=True)
-        self.value = False
-        self.stop()
-
 class SetLangButton(nextcord.ui.View):
     def __init__(self, user):
         super().__init__()
