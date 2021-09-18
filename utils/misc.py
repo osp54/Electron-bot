f6b031a748ac9fbfede465f670fcacac016bc7f1
OSP <76648940+osp54@users.noreply.github.com>
2021-09-18 12:34:42 +0300

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -35,25 +35,3 @@ def error(desc):
 
 def format_name(name: str) -> str:
     return name.replace("_", " ").title().strip()
-
-class Confirm(nextcord.ui.View):
-    def __init__(self, user):
-        super().__init__()
-        self.value = None
-        self.user = user
-
-    @nextcord.ui.button(emoji='✅', style=nextcord.ButtonStyle.green)
-    async def confirm(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        if interaction.user.id != self.user.id:
-            return await interaction.response.send_message('Ты не автор команды!', ephemeral=True)
-        await interaction.response.send_message('Готово.', ephemeral=True)
-        self.value = True
-        self.stop()
-
-    @nextcord.ui.button(emoji='❌', style=nextcord.ButtonStyle.gray)
-    async def cancel(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        if interaction.user.id != self.user.id:
-            return await interaction.response.send_message('Ты не автор команды!', ephemeral=True)
-        await interaction.response.send_message('Отклонено.', ephemeral=True)
-        self.value = False
-        self.stop()
