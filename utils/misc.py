5eecef34a756820f9716978f9dae6dbdb79cf050
OSP <76648940+osp54@users.noreply.github.com>
2021-09-19 11:13:05 +0300

    Я хз

--- utils/misc.py
+++ utils/misc.py
@@ -25,7 +25,7 @@ def get_prefix2(client, message):
 def get_lang(client, message):
     cur.execute(f"SELECT lang FROM guild WHERE ID = {message.guild.id}")
     result = cur.fetchone()
-    if result is None:
+    if result[0] is None:
         return "en"
     if result is not None:
         return result[0]
