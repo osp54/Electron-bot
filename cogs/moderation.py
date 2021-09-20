99ee998c14deaae52fba1bd3012cac2d3ffa8386
OSP <76648940+osp54@users.noreply.github.com>
2021-09-20 18:27:24 +0300

    Localized mute, unmute, kick

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -5,55 +5,53 @@ from nextcord.ext.commands import cooldown, BucketType
 class moderation(commands.Cog, name="moderation"):
     def __init__(self, bot):
         self.bot = bot
+        self.conn = sqlite3.connect(r'db/electron.db')
+        self.b = ConfigParser() # b - bundle
     @commands.command(
         name="mute",
-        usage="`mute [участник] <причина>`",
         aliases=['мьют', 'мут']
     )
     @commands.cooldown(1, 2, commands.BucketType.user)
     @commands.bot_has_permissions(manage_roles=True)
     @commands.has_permissions(manage_roles=True)
     async def mute(self,ctx, member: nextcord.Member, *, reason=None):
-        """
-        Замьютить пользователя на сервере.
-        """
-        view = Delete(ctx.message.author)
         guild = ctx.guild
+        self.b.read(f"locales/{get_lang(self.bot, ctx.message)}.ini")
         mutedRole = nextcord.utils.get(guild.roles, name="Muted")
         if ctx.author.id == member.id:
             embed = nextcord.Embed(
-                title="Ошибка",
-                description="Зачем? Зачем ты хочешь замьютить самого себя?",
+                title=self.b.get('Bundle', 'embed.error'),
+                description=self.b.get('Bundle', 'embed.error.mute.why-mute-myself'),
                 color=0xE02B2B
             )
             await ctx.message.add_reaction('❌')
             return await ctx.send(embed=embed)
         if member.guild_permissions.administrator:
             embed = nextcord.Embed(
-                title="Ошибка",
-                description="У пользователя есть права администратора.",
+                title=self.b.get('Bundle', 'embed.error'),
+                description=self.b.get('Bundle', 'embed.error.mute.user-has-admin-perm'),
                 color=0xE02B2B
             )
             await ctx.message.add_reaction('❌')
             return await ctx.send(embed=embed)
         if member.top_role.position >= ctx.author.top_role.position:
             embed = nextcord.Embed(
-                title='Ошибка',
-                description='Вы не можете замьютить этого пользователя, так как его роль выше или на равне с вашей.',
+                title=self.b.get('Bundle', 'embed.error'),
+                description=self.b.get('Bundle', 'embed.error.mute.role-above-or-equal'),
                 color=0xE02B2B
             )
             await ctx.message.add_reaction('❌')
             return await ctx.send(embed=embed)
         if member.guild_permissions > ctx.author.guild_permissions:
             embed = nextcord.Embed(
-                title='Ошибка',
-                description='Вы не можете замьютить этого пользователя, так как его права выше чем ваши',
+                title=self.b.get('Bundle', 'embed.error'),
+                description=self.b.get('Bundle', 'embed.error.mute.role-above'),
                 color=0xE02B2B
             )
             await ctx.message.add_reaction('❌')
             return await ctx.send(embed=embed)
         if not mutedRole:
-            mutedRole = await guild.create_role(name="Muted")
+            mutedRole = await guild.create_role(name="Electron Mute")
 
             for channel in guild.channels:
                 await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, read_messages=False)
@@ -62,80 +60,67 @@ class moderation(commands.Cog, name="moderation"):
         except:
             return
         embed = nextcord.Embed(
-            title="Успешно!",
-            description=f"**{member.name}** замьючен модератором **{ctx.message.author}**",
+            title=self.b.get('Bundle', 'embed.succerfully'),
+            description=self.b.get('Bundle', 'embed.mute.description').format(member.name),
             color=0x42F56C
         )
-        embed.add_field(name="Причина:", value=reason, inline=False)
+        embed.add_field(name=self.b.get('Bundle', 'embed.moderator'), value=ctx.message.author)
+        embed.add_field(name=self.b.get('Bundle', 'embed.reason'), value=reason, inline=False)
         await ctx.send(embed=embed)
         await ctx.message.add_reaction('✅')
-        await member.send(f"Вы были замьючены в: {ctx.message.guild} причина: {reason}")
+        await member.send(self.b.get('Bundle', 'mute.pm-message'))
     @commands.command(
         name="unmute",
-        usage="`unmute [участник]`",
         aliases=['размьют', 'размут']
     )
     @commands.cooldown(1, 2, commands.BucketType.user)
     @commands.bot_has_permissions(manage_roles=True)
     @commands.has_permissions(manage_roles=True)
     async def unmute(self,ctx, member: nextcord.Member):
-        """
-        Размьютить пользователя на сервере.
-        """
+        self.b.read(f"locales/{get_lang(self.bot, ctx.message)}.ini")
         if ctx.author.id == member.id:
             embed = nextcord.Embed(
-                title="Ошибка",
-                description="Размьютить самого себя не получится.",
+                title=self.b.get('Bundle', 'embed.error'),
+                description=self.b.get('Bundle', 'embed.error.unmute.not-unmute-myself'),
                 color=0xE02B2B
             )
             await ctx.send(embed=embed)
             await ctx.message.add_reaction('❌')
             return
-        mutedRole = nextcord.utils.get(ctx.guild.roles, name="Muted")
+        mutedRole = nextcord.utils.get(ctx.guild.roles, name="Electron Mute")
         try:
             await member.remove_roles(mutedRole)
         except:
             return
         embed = nextcord.Embed(
-            title="Успешно!",
-            description=f"**{member.name}** размьючен модератором **{ctx.message.author}**",
+            title=self.b.get('Bundle', 'embed.succerfully'),
+            description=self.b.get('Bundle', 'embed.unmute.unmuted').format(member),
             color=0x42F56C
-        )
+        ).add_field(name=self.b.get('Bundle', 'embed.moderator'), value=ctx.message.author)
         await ctx.send(embed=embed)
-        await member.send(f"Вы были размьючены в {ctx.message.guild}!")
+        await member.send(self.b.get('Bundle', 'embed.unmute.pm-message').format(ctx.message.guild))
         await ctx.message.add_reaction('✅')
     @commands.command(
         name='kick',
-        usage="`kick [участник] <причина>`",
         aliases=['кикнуть', 'кик', 'вигнать']
         )
     @commands.cooldown(1, 2, commands.BucketType.user)
     @commands.bot_has_permissions(kick_members=True)
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member: nextcord.Member, *, reason="None"):
-        """
-        Кикнуть пользователя из сервера.
-        """
+        self.b.read(f"locales/{get_lang(self.bot, ctx.message)}.ini")
         if ctx.author.id == member.id:
             embed = nextcord.Embed(
-                title="Ошибка",
-                description="Зачем? Зачем ты хочешь кикнуть самого себя?",
-                color=0xE02B2B
-            )
-            await ctx.message.add_reaction('❌')
-            return await ctx.send(embed=embed)
-        if member.guild_permissions.administrator:
-            embed = nextcord.Embed(
-                title="Ошибка",
-                description="У пользователя есть права администратора.",
+                title=self.b.get('Bundle', 'embed.error'),
+                description=self.b.get('Bundle', 'embed.error.kick.why-kick-myself'),
                 color=0xE02B2B
             )
             await ctx.message.add_reaction('❌')
             return await ctx.send(embed=embed)
         if member.top_role.position >= ctx.author.top_role.position:
             embed = nextcord.Embed(
-                title='Ошибка',
-                description='Вы не можете выгнать этого пользователя, так как его роль выше или на равне с вашей.',
+                title=self.b.get('Bundle', 'embed.error'),
+                description=self.b.get('Bundle', 'embed.error.kick.role-above-or-equal'),
                 color=0xE02B2B
             )
             await ctx.message.add_reaction('❌')
@@ -143,36 +128,34 @@ class moderation(commands.Cog, name="moderation"):
         if member.guild_permissions > ctx.author.guild_permissions:
             embed = nextcord.Embed(
                 title='Ошибка',
-                description='Вы не можете выгнать этого пользователя, так как его права выше чем ваши',
+                description=self.b.get('Bundle', 'embed.error.kick.role-above'),
                 color=pxE02B2B
             )
             await ctx.message.add_reaction('❌')
             return await ctx.send(embed=embed)
         await member.kick(reason=f"{reason}({ctx.message.author})")
         embed = nextcord.Embed(
-            title="Успешно!",
-            description=f"**{member}** кикнут модератором **{ctx.message.author}**!",
+            title=self.b.get('Bundle', 'embed.succerfully'),
+            description=self.b.get('Bundle', 'embed.kick.kicked').format(member),
             color=0x42F56C
-        )
-        embed.add_field(
-            name="Причина:",
+        ).add_field(
+            name=self.b.get('Bundle', 'embed.moderator'),
+            value=ctx.message.author
+        ).add_field(
+            name=self.b.get('Bundle', 'embed.reason'),
             value=reason
         )
         await ctx.send(embed=embed)
-        await member.send(f"Вы были кикнуты в {ctx.message.guild}, модератором {ctx.message.author}!")
+        await member.send(self.b.get('Bundle', 'kick.pm-message').format(ctx.message.guild, ctx.message.author))
         await ctx.message.add_reaction('✅')
     @commands.command(
         name="ban",
-        usage="`ban [участник] <причина>`",
         aliases=['бан']
     )
     @commands.cooldown(1, 2, commands.BucketType.user)
     @commands.bot_has_permissions(ban_members=True)
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member: nextcord.Member, *, reason="Причина не написана."):
-        """
-        Забанить пользователя на сервере.
-        """
         if ctx.author.id == member.id:
             embed = nextcord.Embed(
                 title="Ошибка",
