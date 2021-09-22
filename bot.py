c91a3f9a7e1688caf43433dbf0810915ce6557fe
OSP <76648940+osp54@users.noreply.github.com>
2021-09-22 18:22:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,6 +73,6 @@ if __name__ == "__main__":
     load_extensions("./cogs") #когсы командc
     client.load_extension("jishaku")
 try:
-    client.run(ODYxNTQxMjg3MTYxMTAyMzc2.YOLS2Q.ylwKDaLJE4BypVzaLB6Hwai9GHw)
+    client.run('ODYxNTQxMjg3MTYxMTAyMzc2.YOLS2Q.ylwKDaLJE4BypVzaLB6Hwai9GHw')
 except KeyboardInterrupt as k:
     info("KeyboardInterrupt")
