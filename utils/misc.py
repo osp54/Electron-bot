8e80b77bf2cdb71b4cf99167613f6077d7a30f92
OSP <76648940+osp54@users.noreply.github.com>
2021-09-26 21:07:20 +0300

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -18,6 +18,7 @@ def get_prefix2(client, message):
 def get_lang(message):
     res = collg.find_one("_id": message.guild.id)
     return res["lang"]
+
 def localize(ctx, self, to_local, bundle="Bundle"):
     self.b.read(f"locales/{get_lang( ctx.message)}.ini")
     try:
@@ -25,6 +26,30 @@ def localize(ctx, self, to_local, bundle="Bundle"):
     except:
         self.b.read(f"locales/en.ini")
         return self.b.get(bundle, to_local)
+
+#загрузить все расширения из папки
+def load_extensions(dir):
+    for file in os.listdir(dir):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            try:
+                client.load_extension(f"{dir[2:]}.{extension}")
+                info(f"Loaded extension {Fore.BLUE}{extension}")
+            except Exception as e:
+                exception = f"{type(e).__name__}: {e}"
+                error(f"Failed to load extension {Fore.BLUE}{extension}{Fore.RESET}\n{exception}")
+
+def unload_extensions(dir):
+    for file in os.listdir(dir):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            try:
+                client.unload_extension(f"{dir[2:]}.{extension}")
+                info(f"Unloaded extension {Fore.BLUE}{extension}")
+            except Exception as e:
+                exception = f"{type(e).__name__}: {e}"
+                error(f"Failed to unload extension {Fore.BLUE}{extension}{Fore.RESET}\n{exception}")
+
 def info(desc):
     now = pendulum.now('Europe/Moscow')
     print(f"{Fore.WHITE}[{now.day}:{now.hour}:{now.minute}:{now.second}] " + Fore.BLUE + f"[I] {Fore.RESET}" + desc)
