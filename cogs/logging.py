e6eddbbdd8c039f261bcd47de008ead986312ea1
OSP <76648940+osp54@users.noreply.github.com>
2021-09-28 08:46:17 +0300

    Update logging.py

--- cogs/logging.py
+++ cogs/logging.py
@@ -11,5 +11,10 @@ class logging(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
         info(f"Removed guild. Name: {Fore.CYAN}{guild.name}{Fore.RESET}, members: {Fore.CYAN}{guild.member_count}")
+    @commands.Cog.listener()
+    async def on_command(self, ctx):
+        channel = self.bot.get_channel(888113288586625064)
+        cmd = ctx.command.qualified_name
+        channel.send(f"Executed command {cmd}\nUser: {ctx.author}\nGuild: {ctx.guild.name} | (ctx.guild.id)")
 def setup(bot):
     bot.add_cog(logging(bot))
