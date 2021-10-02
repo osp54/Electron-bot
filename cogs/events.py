bf39aeeac9283304e097bc07c488080b43d3af23
OSP <76648940+osp54@users.noreply.github.com>
2021-10-03 00:08:25 +0300

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -71,10 +71,11 @@ class events(commands.Cog):
                 ).add_field(name=self.b.get('Bundle', 'embed.required-rights'), value=f"```\n{missingperms}\n```")
             )
         elif isinstance(error, commands.CommandOnCooldown):
-            await ctx.send(embed = nextcord.Embed(
-                title=self.b.get('Bundle', 'embed.error')
-                description=self.b.get('Bundle', 'embed.error.command-in-cooldown').format(error.retry_after:.2f), 
-                color=0xFF0000
+            await ctx.send(
+                embed = nextcord.Embed(
+                    title=self.b.get('Bundle', 'embed.error'),
+                    description=self.b.get('Bundle', 'embed.error.command-in-cooldown').format(error.retry_after:.2f), 
+                    color=0xFF0000
                 )
             )
         elif isinstance(error, commands.BotMissingPermissions):
