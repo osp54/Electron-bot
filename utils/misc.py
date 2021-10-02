d6814c137f6f2cdf55ecc4696c839c2f9f8dc90c
OSP <76648940+osp54@users.noreply.github.com>
2021-10-02 23:25:40 +0300

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -8,6 +8,7 @@ from nextcord.ext.commands import when_mentioned_or
 from configparser import ConfigParser
 from colorama import init, Fore, Back, Style
 
+cp
 mclient = pymongo.MongoClient("mongodb+srv://electron:W$2ov3b$Fff58ludgg@cluster.xyknx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = mclient.electron
 collg = db.guilds
@@ -20,37 +21,37 @@ def get_lang(message):
     res = collg.find_one({"_id": message.guild.id})
     return res["lang"]
 
-def localize(ctx, self, to_local, bundle="Bundle"):
-    self.b.read(f"locales/{get_lang( ctx.message)}.ini")
+def localize(ctx, to_local, bundle="Bundle"):
+    cp.read(f"locales/{get_lang(ctx.message)}.ini")
     try:
-        return self.b.get(bundle, to_local)
+        return cp.get(bundle, to_local)
     except:
-        self.b.read(f"locales/en.ini")
+        cp.read(f"locales/en.ini")
         return self.b.get(bundle, to_local)
 async def cmdInfo(ctx, self, cmd):
-        ConfigParser().read(f"locales/{get_lang(ctx.message)}.ini")
+        cp.read(f"locales/{get_lang(ctx.message)}.ini")
         try:
             cmd = self.bot.get_command(cmd)
         except:
             eembed = nextcord.Embed(
-                title=self.b.get("Bundle", "embed.error"),
-                description=self.b.get("Bundle", "error.embed.command.not.found"),
+                title=cp.get("Bundle", "embed.error"),
+                description=cp.get("Bundle", "error.embed.command.not.found"),
                 color=0xFF0000
             )
             return await ctx.send(embed=eembed)
         embed = nextcord.Embed(
             title=cmd.name.capitalize(),
-            description=self.b.get("Bundle", f"{cmd}.description"),
+            description=cp.get("Bundle", f"{cmd}.description"),
             color=0x2B95FF
         ).add_field(
-            name=self.b.get("Bundle", "embed.help.usage"),
-            value=self.b.get("Bundle", f"{cmd}.usage")
+            name=cp.get("Bundle", "embed.help.usage"),
+            value=cp.get("Bundle", f"{cmd}.usage")
         )
         aliase = '('
         for alias in cmd.aliases:
             aliase += f" `{alias}` "
         embed.add_field(
-            name=self.b.get("Bundle", "embed.help.aliases"),
+            name=cp.get("Bundle", "embed.help.aliases"),
             value=f"{aliase})"
         )
         await ctx.send(embed=embed)
