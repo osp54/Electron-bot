eb517c9b0fdf5de14cd1313fa570833f9cd48c00
OSP <76648940+osp54@users.noreply.github.com>
2021-10-03 20:50:21 +0300

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -16,7 +16,7 @@ class events(commands.Cog):
         self.collg = self.mclient.electron.guilds
     @commands.Cog.listener('on_message')
     async def on_bot_mention(self, message):
-        self.b.read(f"locales/{get_lang(ctx.message)}.ini")
+        self.b.read(f"locales/{get_lang(message)}.ini")
         if '<@861541287161102376>' == message.content:
             prefix = get_prefix2(self.bot, message, True)
             await message.reply(self.b.get('Bundle', 'HelloMessage').format(prefix), mention_author=True)
