2c30ae6fe082ab0ea993ef0ebeedc2d43c0bd589
OSP <76648940+osp54@users.noreply.github.com>
2021-10-03 21:40:18 +0300

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,5 +1,6 @@
 import nextcord
 import sqlite3
+from utils.misc import get_lang
 from configparser import ConfigParser
 from nextcord.ext import commands
 from nextcord.ext.commands import cooldown, BucketType
