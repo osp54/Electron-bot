128140dfdeb7451df79a3cac88a9dfe9051c2c76
OSP <76648940+osp54@users.noreply.github.com>
2021-10-05 13:05:33 +0300

    Configuration cog, `config mute_role` command

---
+++ cogs/config.py
@@ -0,0 +1,33 @@
+import pymongo
+import nextcord
+from utils.misc import get_lang
+from nextcord.ext import commands
+from configparser import ConfigParser
+
+class config(commands.Cog, name="config"):
+    def __init__(self, bot):
+        self.bot = bot
+        self.mclient = pymongo.MongoClient("mongodb+srv://electron:W$2ov3b$Fff58ludgg@cluster.xyknx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        self.collg = self.mclient.electron.guilds
+        self.b = ConfigParser() # b - bundle
+   @commands.group(name="config", invoke_without_command=True)
+   async def config(self, ctx):
+       self.b.read(f"locales/{get_lang(ctx.message)}.ini")
+       embed = discord.Embed(
+           title=self.b.get('Bundle', 'embed.config.info.title'),
+           description=self.b.get('Bundle', 'embed.config.info.desc').format("\n"),
+           color=0x42F56C
+       )
+       await ctx.send(embed=embed)
+   @config.command(name="mute_role")
+   async def mute_role(self, ctx, role: nextcord.Role):
+       self.b.read(f"locales/{get_lang(ctx.message)}.ini")
+       self.collg.update_one({"_id": ctx.guild.id}, {"$set": {'mute_role': role.id}})
+       embed = nextcord.Embed(
+           title=self.b.get('Bundle', 'embed.succerfully'),
+           description=self.b.get('Bundle', 'embed.mute-role-changed').format(role.mention),
+           color=0x42F56C
+       )
+       await ctx.send(embed=embed)
+def setup(bot):
+    bot.add_cog(config(bot))
