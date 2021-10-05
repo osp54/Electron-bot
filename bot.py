a88f74ad5bb93fadff3cf0ad20e34d4da62c90d5
OSP <76648940+osp54@users.noreply.github.com>
2021-10-05 18:21:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ async def on_ready():
     for guild in client.guilds:
         if collg.count_documents({"_id": guild.id}) == 0:
             res = collg.insert_one({"_id": guild.id, "lang": "en", "prefix": "$"})
-            modcount += res.matched_count
+            modcount += 1
     tEnd = time.time()
     tElapsed = tEnd - tStart
     await client.change_presence(activity=nextcord.Game(name=f"$help | Guilds: {len(client.guilds)}"))
