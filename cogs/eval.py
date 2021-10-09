0fabebb961c0bc3ca2b525c7699348681f0cf3ea
OSP <76648940+osp54@users.noreply.github.com>
2021-10-09 12:31:10 +0300

    Update eval.py

--- cogs/eval.py
+++ cogs/eval.py
@@ -12,24 +12,6 @@ class EvalCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    def resolve_variable(self, variable):
-        if hasattr(variable, "__iter__"):
-            var_length = len(list(variable))
-            if (var_length > 100) and (not isinstance(variable, str)):
-                return f"<a {type(variable).__name__} iterable with more than 100 values ({var_length})>"
-            elif (not var_length):
-                return f"<an empty {type(variable).__name__} iterable>"
-        
-        if (not variable) and (not isinstance(variable, bool)):
-            return f"<an empty {type(variable).__name__} object>"
-        return (variable if (len(f"{variable}") <= 1000) else f"<a long {type(variable).__name__} object with the length of {len(f'{variable}'):,}>")
-    
-    def prepare(self, string):
-        arr = string.strip("```").replace("py\n", "").replace("python\n", "").split("\n")
-        if not arr[::-1][0].replace(" ", "").startswith("return"):
-            arr[len(arr) - 1] = "return " + arr[::-1][0]
-        return "".join(f"\n\t{i}" for i in arr)
-    
     @commands.command(pass_context=True, aliases=['eval', 'exec', 'evaluate'])
     @commands.is_owner()
     async def _eval(self, ctx, *, code: str):
