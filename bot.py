54fa1c080337af5d06e30692d5f0d6e319b437c5
OSP <76648940+osp54@users.noreply.github.com>
2021-10-10 09:37:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ async def on_ready():
     tEnd = time.time()
     tElapsed = tEnd - tStart
     await client.change_presence(activity=nextcord.Game(name=f"$help | Guilds: {len(client.guilds)}"))
-    info(f"Added new {modcount} guilds to database")
+    info(f"Added new {Fore.BLUE}{modcount}{Fore.RESET} guilds to database" if modcount != 0 else "")
     info(f"Logged in as {Fore.BLUE}{client.user.name}{Fore.RESET}, Guilds: {Fore.BLUE}{len(client.guilds)}")
     info(f"NextCord.py version: {Fore.BLUE}{nextcord.__version__}")
     info(f"Python version: {Fore.BLUE}{platform.python_version()}")
