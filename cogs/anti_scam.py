5ba0be63fdc796b265f56f55ed6a3c2451dcb454
osp54 <clashmekers54@gmail.com>
2021-10-14 16:21:58 +0300

    Anti-Scam

--- cogs/anti_scam.py
+++ cogs/anti_scam.py
@@ -2,7 +2,8 @@ import nextcord
 from nextcord.ext import commands
 
 scam_links = ["steamcommunity.link", "discorcl.link", "steamcommunity.com.ru", "steamcommunnity.ru", "steamccommunity.com.ru", "steamcommuunity.com.ru", "surl.li", "stearncornmrunity.ru.com", "fustcup.ru", "steamcommrunity.com", "brapo.space", "steamcommunutiy.com", "discord.giveawey.com"]
-bad_words = ["free nitro", "бесплатное нитро", "nitro free", "нитро бесплатно"]
+bad_words = ["free nitro", "бесплатное нитро", "nitro free", "нитро бесплатно", "раздача бесплатного нитро", "раздача нитро", "нитро раздача", "нитро раздача бесплатно", "nitro distribution", "distribution nitro"]
+bad_domains = [".ru.com", ".link", ".gift"]
 
 class anti_scam(commands.Cog):
     def __init__(self, bot):
@@ -11,8 +12,13 @@ class anti_scam(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         for word in bad_words:
-            if word in message.content.lower() and "https" in message.content.lower() or "http:" in message.content.lower():
+            if word in message.content.lower() and "https://" in message.content.lower() or "http://" in message.content.lower():
                 await message.reply(f"WARNING! Scam detected! Ban this member or give punishment! Member: {message.author}, id: {message.author.id}")
+        msg = message.content.split()
+        for m in msg:
+            for d in bad_domains:
+                if m.startswith("https://disc") or m.startswith("http://disc") and m.endswith(d):
+                     await message.reply(f"WARNING! Scam detected! Ban this member or give punishment! Member: {message.author}, id: {message.author.id}")
         for link in scam_links:
             if link in message.content.lower():
                 try:
