94e99ed3512da150af1b13c097eb86c93b0fcedd
osp54 <clashmekers54@gmail.com>
2021-10-14 16:24:57 +0300

    Anti-Scam fic

--- cogs/anti_scam.py
+++ cogs/anti_scam.py
@@ -13,19 +13,19 @@ class anti_scam(commands.Cog):
     async def on_message(self, message):
         for word in bad_words:
             if word in message.content.lower() and "https://" in message.content.lower() or "http://" in message.content.lower():
-                await message.reply(f"WARNING! Scam detected! Ban this member or give punishment! Member: {message.author}, id: {message.author.id}")
+                return await message.reply(f"WARNING! Scam detected! Ban this member or give punishment! Member: {message.author}, id: {message.author.id}")
         msg = message.content.split()
         for m in msg:
             for d in bad_domains:
                 if m.startswith("https://disc") or m.startswith("http://disc") and m.endswith(d):
-                     await message.reply(f"WARNING! Scam detected! Ban this member or give punishment! Member: {message.author}, id: {message.author.id}")
+                     return await message.reply(f"WARNING! Scam detected! Ban this member or give punishment! Member: {message.author}, id: {message.author.id}")
         for link in scam_links:
             if link in message.content.lower():
                 try:
                     await message.delete()
-                    await ctx.send(f"I deleted the post with the scam link. I recommend banning or punishing this member. Member: {message.author}, id: {message.author.id}")
+                    return await ctx.send(f"I deleted the post with the scam link. I recommend banning or punishing this member. Member: {message.author}, id: {message.author.id}")
                 except:
-                    await message.reply(f"WARNING! Scam link detected! Ban this member or give punishment! Member: {message.author}, id: {message.author.id}")
+                    return await message.reply(f"WARNING! Scam link detected! Ban this member or give punishment! Member: {message.author}, id: {message.author.id}")
   
 def setup(bot):
     bot.add_cog(anti_scam(bot))
