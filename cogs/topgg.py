3dc99760154d09131bb319de67c48086d53f9c0e
osp54 <clashmekers54@gmail.com>
2021-10-23 16:31:27 +0300

    fix

--- cogs/topgg.py
+++ cogs/topgg.py
@@ -1,18 +1,18 @@
 import nextcord
-import topggpy
+import topgg
 from nextcord.ext import commands, tasks
 from utils.misc import info, error
 
 class _topgg(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.bot.topggpy = topgg.DBLClient(self.bot, "https://top.gg/bot/861541287161102376/webhooks")
+        self.topggpy = topgg.DBLClient(self.bot, "https://top.gg/bot/861541287161102376/webhooks")
 
     @tasks.loop(minutes=30)
     async def update_stats(self):
         try:
-            await self.bot.topggpy.post_guild_count()
-            info(f"[Topgg]Posted server count ({self.bot.topggpy.guild_count})")
+            await self.topggpy.post_guild_count()
+            info(f"[Topgg]Posted server count ({self.topggpy.guild_count})")
         except Exception as e:
             error(f"[Topgg]Failed to post server count\n{e.__class__.__name__}: {e}")
     @commands.Cog.listener()
