05e58a60e85e8f732090d34f6e48adc9a33c03ac
osp54 <clashmekers54@gmail.com>
2021-11-20 15:50:02 +0200

    fix

--- utils/mongo.py
+++ utils/mongo.py
@@ -4,8 +4,7 @@ from utils.console import error, info
 
 class MongoM():
     def __init__(self, coll = "guilds"):
-        self.con_str = "mongodb://electron:W$2ov3b$Fff58ludgg@localhost:27017/?authSource=admin"
-        self.client = motor.motor_asyncio.AsyncIOMotorClient(self.con_str)
+        self.client = motor.motor_asyncio.AsyncIOMotorClient("localhost", 27017)
         self.coll = self.client["electron"][coll]
     async def connect(self):
         try:
