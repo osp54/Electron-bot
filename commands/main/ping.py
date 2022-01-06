4fa1ac40b2ebfff6b79e583ed7b5a1a70e69f96b
osp54 <clashmekers54@gmail.com>
2022-01-06 10:33:10 +0200

    advanced ping

--- commands/main/ping.py
+++ commands/main/ping.py
@@ -1,3 +1,4 @@
+import time
 from configparser import ConfigParser
 from utils.bot import get_lang
 from nextcord.ext import commands
@@ -9,7 +10,10 @@ bot: commands.Bot = ""
 @commands.cooldown(1, 2, commands.BucketType.user)
 async def ping(ctx):
     b.read(f"locales/{await get_lang(ctx.message)}.ini")
-    await ctx.send(b.get('Bundle', 'ping').format(round(bot.latency * 1000)))
+    start_time = time.time()
+    message = await ctx.send(b.get('Bundle', 'ping.check'))
+    end_time = time.time()
+    await message.edit(b.get('Bundle', 'ping').format(round(bot.latency * 1000), round((end_time - start_time) * 1000)))
 
 def setup(_bot):
     global bot
